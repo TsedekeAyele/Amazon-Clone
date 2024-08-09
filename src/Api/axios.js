a3777e1c58c4c9a147a6016f663a3bd9b23532e7
TsedekeAyele <tsedekedeve39@gmail.com>
2024-08-09 23:50:44 +0300

    payment adust commit

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // local instance of firebase functions
-  baseURL: "http://127.0.0.1:5001/clone-6116a/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-6116a/us-central1/api",
 // deployed version of amazon_server on render.com
   baseURL: "https://amazon-api-deploy-jhp4.onrender.com/",
 });
